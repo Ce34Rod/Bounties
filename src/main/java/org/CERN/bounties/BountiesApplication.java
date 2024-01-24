d7fcbb44c1132fe699955a80e5b4087666570be6
Ce34Rod <139182790+Ce34Rod@users.noreply.github.com>
2024-01-23 22:39:10 -0600

    Adding more Spring

---
+++ src/main/java/org/CERN/bounties/BountiesApplication.java
@@ -0,0 +1,12 @@
+package org.CERN.bounties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BountiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BountiesApplication.class, args);
+		}
+}
