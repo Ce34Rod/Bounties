49fcb8aef9b5b2a0e27a75508d54cd27d0253a1a
Ce34Rod <139182790+Ce34Rod@users.noreply.github.com>
2024-01-30 14:32:18 -0600

    Making bounty objects

--- src/main/java/org/CERN/bounties/models/Bounty.java
+++ src/main/java/org/CERN/bounties/models/Bounty.java
@@ -1,4 +1,15 @@
 package org.CERN.bounties.models;
 
-public class Bounty {
+public class Bounty extends AbstractEntity {
+
+    private static String name;
+
+    private static int bounty;
+
+    private static String details;
+
+    private static String location;
+
+
+
 }
