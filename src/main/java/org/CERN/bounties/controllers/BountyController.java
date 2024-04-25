4ec3feaa9d0d890677253c028dea0ce1c45e1809
Ce34Rod <139182790+Ce34Rod@users.noreply.github.com>
2024-04-24 23:29:06 -0500

    Name input

--- src/main/java/org/CERN/bounties/controllers/BountyController.java
+++ src/main/java/org/CERN/bounties/controllers/BountyController.java
@@ -1,4 +1,40 @@
 package org.CERN.bounties.controllers;
 
+import jakarta.validation.Valid;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class BountyController {
+
+
+    @GetMapping("")
+    public String createRecord(Model model){
+        model.addAttribute(new Record());
+        model.addAttribute(new DataPoint());
+        return "records/create";
+    }
+
+    @PostMapping("create")
+    public String processCreateRecord(@ModelAttribute @Valid Record record, @ModelAttribute DataPoint dataPoint){
+
+//        dataPointRepository.save(dataPoint);
+//        Record record1 = new Record(record.getName(), dataPoint);
+//        recordRepository.save(record1);
+//        dataPoint.setRecord(record1);
+        dataPointRepository.save(dataPoint);
+
+        Record record1 = new Record(record.getName(), dataPoint);
+        recordRepository.save(record1);
+
+        // Set the Record in the DataPoint
+        dataPoint.setRecord(record1);
+
+        // Set the DataPoint in the Record
+        record1.getDataPointsList().add(dataPoint);
+
+
+        return "redirect:/records/index";
+    }
 }
